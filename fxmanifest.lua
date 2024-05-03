d5810a63311bd7543a8673ff9a8a81d0f24bcec4
COOL <yigitgurses2012@gmail.com>
2024-05-03 15:04:07 +0300

    Create

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'cool0356'
+description 'Intuitive lib system by Nuxt Lab.'
+version '1.0.0'
+
+client_script 'client/*.lua'
+
+server_script 'server/*.lua'
+
+shared_script 'shared/*.lua'
+
+lua54 'yes'
