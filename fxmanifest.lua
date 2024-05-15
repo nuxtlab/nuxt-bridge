6f824059d3ddbc158cc3f7054d6325de5001d252
COOL <yigitgurses2012@gmail.com>
2024-05-15 17:05:46 +0300

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,18 @@ game 'gta5'
 
 author 'cool0356'
 description 'Intuitive bridge system by Nuxt Lab.'
-version '0.0.4'
+version '0.0.5'
 
 files {
     'collection/*.json',
     'data/*.lua',
+    'dist/*',
+	'dist/**/*',
     'locale/*.lua',
     'module/**/*.lua'
 }
+
+ui_page 'dist/index.html'
 
 shared_script 'shared/*.lua'
 
